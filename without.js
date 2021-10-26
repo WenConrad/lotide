581e1034fac3e75c525243644563d8326a478c72
WenConrad <conradwen@gmail.com>
2021-10-26 16:35:11 -0700

    remove test condition and lint without.js

--- without.js
+++ without.js
@@ -1,20 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertEqual = function(actual, expected) {
-  console.log((eqArrays(actual, expected)) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const without = function(sources, itemsToRemove) {
   let removed = [];
   let inItemsToRemove = false;
