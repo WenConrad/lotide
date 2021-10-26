feb292ff9fb13efe4285c0334aa6395a18173996
WenConrad <conradwen@gmail.com>
2021-10-26 13:55:00 -0700

    linted and remove test conditions

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,5 @@
-const assertEqual = function(actual, expected) {
-  console.log((actual === expected) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function(arr1, arr2) {
-  if(arr1.length === arr2.length) {
+  if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         return false;
@@ -14,9 +10,3 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
