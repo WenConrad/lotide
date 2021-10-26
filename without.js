1b20ff4f43f9a6a5a316c510ebe29b246a9e4cb9
WenConrad <conradwen@gmail.com>
2021-10-26 16:33:32 -0700

    done :^)

--- without.js
+++ without.js
@@ -15,4 +15,19 @@ const assertEqual = function(actual, expected) {
   console.log((eqArrays(actual, expected)) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual(1,1);
+const without = function(sources, itemsToRemove) {
+  let removed = [];
+  let inItemsToRemove = false;
+  for (let i = 0; i < sources.length; i++) {
+    for (let itemToRemove of itemsToRemove) {
+      if (sources[i] === itemToRemove) {
+        inItemsToRemove = true;
+      }
+    }
+    if (!inItemsToRemove) {
+      removed.push(sources[i]);
+    }
+    inItemsToRemove = false;
+  }
+  return removed;
+};
