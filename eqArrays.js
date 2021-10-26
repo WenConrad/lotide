6ec288cd7b15262d588a5e8a2f6151f7475c6dbb
WenConrad <conradwen@gmail.com>
2021-10-26 13:49:35 -0700

    logic check

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,22 @@
 const assertEqual = function(actual, expected) {
   console.log((actual === expected) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const eqArrays = function(arr1, arr2) {
+  if(arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
