40050f44507582c56d2a02b74cb6c0231fd781ce
WenConrad <conradwen@gmail.com>
2021-10-27 19:48:53 -0700

    call eqObjects into assertObjectsEqual, lint code

---
+++ assertObjectsEqual.js
@@ -0,0 +1,41 @@
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  console.log((eqObjects(actual, expected)) ? `✅️ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}` : `❌️ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const eqObjects = function(obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  } else {
+    for (let key in obj1) {
+      if (Array.isArray(obj1[key])) {
+        if (!eqArrays(obj1[key], obj2[key])) {
+          return false;
+        }
+      } else if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2);
