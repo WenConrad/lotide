26eb60b25862841a717365e01269cf4badc1ebb2
WenConrad <conradwen@gmail.com>
2021-10-27 19:04:27 -0700

    copy eqArrays into eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,20 @@
+const assertEqual = function(actual, expected) {
+  console.log((actual === expected) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const eqObjects = function(obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
@@ -10,3 +27,10 @@ const eqObjects = function(obj1, obj2) {
     return true;
   }
 };
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
