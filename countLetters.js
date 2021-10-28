02c3a68a0943e945518e9ff8dca9dc0a4395c423
WenConrad <conradwen@gmail.com>
2021-10-27 17:33:29 -0700

    set up test case and declare initial function countLetters

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  console.log((actual === expected) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function() {
+  for () {}
+}
+
+
+
+
+
+const testExample = {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+};
+
+assertEqual(countLetters("lighthouse in the house"), testExample);
