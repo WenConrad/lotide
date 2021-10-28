e41d9db4cc173580ce72a7ba697a9b5f104568eb
WenConrad <conradwen@gmail.com>
2021-10-28 15:07:38 -0700

    remove test cases

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -7,6 +5,3 @@ const map = function(array, callback) {
   }
   return results;
 };
-
-const results1 = map(words, word => word[0]);
-console.log(results1);
