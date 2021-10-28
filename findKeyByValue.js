84bc51597e9538228a72e935dbefdf2b05008d72
WenConrad <conradwen@gmail.com>
2021-10-27 18:12:17 -0700

    remove tests and lint findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,7 +1,3 @@
-const assertEqual = function(actual, expected) {
-  console.log((actual === expected) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const findKeyByValue = function(objs, val) {
   for (let key in objs) {
     if (objs[key] === val) {
@@ -9,14 +5,3 @@ const findKeyByValue = function(objs, val) {
     }
   }
 };
-
-
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
