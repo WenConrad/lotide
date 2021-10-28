479c4f45f898e2495a9e8df6cc048d1d7bcad614
WenConrad <conradwen@gmail.com>
2021-10-27 18:32:42 -0700

    remove testers and lint eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,10 +1,6 @@
-const assertEqual = function(actual, expected) {
-  console.log((actual === expected) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqObjects = function(obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return false
+    return false;
   } else {
     for (let key in obj1) {
       if (obj1[key] !== obj2[key]) {
@@ -14,16 +10,3 @@ const eqObjects = function(obj1, obj2) {
     return true;
   }
 };
-
-
-
-
-
-
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(Object.keys(ab).length);
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
