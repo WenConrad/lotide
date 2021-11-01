8800f6216e133b40c341e37e18cc7a337f296348
WenConrad <conradwen@gmail.com>
2021-11-01 12:37:05 -0700

    refactor asertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require("./eqArrays");
 
 const assertEqArrays = function(actual, expected) {
   console.log((eqArrays(actual, expected)) ? `✅️ Assertion Passed: ${actual} === ${expected}` : `❌️ Assertion Failed: ${actual} !== ${expected}`);
