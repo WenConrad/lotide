84a55a4c39ec2e230196eaf8d3f16a128112df86
WenConrad <conradwen@gmail.com>
2021-11-01 13:03:06 -0700

    refactor tests for head middle and tail using mocha and chai

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,4 +1,11 @@
-const assertEqArrays = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
 
-assertEqArrays(tail([1, 2, 3, 4]), [2, 3, 4]);
+describe("#tail", () => {
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it("returns [] for ['1']", () => {
+    assert.deepEqual(tail(["1"]), []);
+  });
+});
